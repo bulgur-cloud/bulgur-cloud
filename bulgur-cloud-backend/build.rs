b9c6578c3ddeddea0eaff690825ee12c32f7ddf9
Kaan Barmore-Genc <kaan@bgenc.net>
2022-09-16 00:49:49 -0400

    Ensure build.rs workaround only runs once

--- bulgur-cloud-backend/build.rs
+++ bulgur-cloud-backend/build.rs
@@ -1,6 +1,9 @@
 use std::{fs, path::PathBuf};
 
 fn main() {
+    // We really only need this to run once, ever.
+    println!("cargo:rerun-if-changed=./Cargo.toml");
+
     // Create the web-build folder, otherwise the embed will throw an error
     // in CI where the folder may be missing.
     fs::create_dir_all(
