699890779a794277f541c3336b8049fa48279358
Kaan Barmore-Genç <kaan@bgenc.net>
2022-12-26 18:41:11 -0500

    Move select, rename, delete for individual folder entries to the action menu (#174)
    Add action button

--- bulgur-cloud-frontend/src/storage/FolderListEntry.tsx
+++ bulgur-cloud-frontend/src/storage/FolderListEntry.tsx
@@ -1,6 +1,5 @@
-import { HStack, Icon, Text } from "native-base";
+import { HStack, Icon, Menu, Pressable, Text } from "native-base";
 import { joinURL } from "../fetch";
-import { FillSpacer } from "../FillSpacer";
 import {
   StorageAction,
   storageSlice,
@@ -15,14 +14,14 @@ import { BLink } from "../BLink";
 
 function itemIconType({
   isFile,
-  isMarkedForMove,
+  isSelected,
   itemName,
 }: {
   isFile: boolean;
-  isMarkedForMove: boolean;
+  isSelected: boolean;
   itemName: string;
 }) {
-  if (isMarkedForMove) return "check";
+  if (isSelected) return "check";
   if (!isFile) return "folder";
   const extensionMatch = /[.]([^.]+)$/.exec(itemName);
   const extension = extensionMatch ? extensionMatch[1] : undefined;
@@ -38,7 +37,7 @@ export function FolderListEntry(
 ) {
   const { item, route } = params;
   const dispatch = useAppDispatch();
-  const isMarkedForMove = useAppSelector(
+  const isSelected = useAppSelector(
     (state) =>
       state.storage.selected[
         joinURL(route.params.store, route.params.path, item.name)
@@ -49,14 +48,13 @@ export function FolderListEntry(
   const { store, path } = route.params;
 
   const selectEntry = () => {
-    console.log("selecting");
     const params = {
       store: route.params.store,
       path: route.params.path,
       name: item.name,
       isFile: item.is_file,
     };
-    if (isMarkedForMove) {
+    if (isSelected) {
       dispatch(storageSlice.actions.unmarkSelected(params));
     } else {
       dispatch(storageSlice.actions.markSelected(params));
@@ -71,13 +69,14 @@ export function FolderListEntry(
         name={itemIconType({
           isFile: item.is_file,
           itemName: item.name,
-          isMarkedForMove,
+          isSelected,
         })}
         color="darkText"
         opacity={isHidden ? 40 : 100}
         onPress={selectEntry}
       />
       <BLink
+        style={{ flexGrow: 1 }}
         screen="Dashboard"
         params={{
           store,
@@ -97,25 +96,112 @@ export function FolderListEntry(
       >
         <Text opacity={isHidden ? 40 : 100}>{item.name}</Text>
       </BLink>
-      <FillSpacer />
-      <Icon
-        as={FontAwesome5}
-        name="pencil-alt"
-        accessibilityLabel="rename"
-        height="100%"
-        size={4}
+      <ActionMenu
+        store={store}
+        path={path}
+        isFile={item.is_file}
+        name={item.name}
+        isSelected={isSelected}
+      />
+    </HStack>
+  );
+}
+
+type ActionMenuProps = {
+  store: string;
+  path: string;
+  name: string;
+  isFile: boolean;
+  isSelected: boolean;
+};
+
+/** The action menu displayed for every folder entry.
+ *
+ * TODO: There should be just one Action Menu component! Rendering this over and
+ * over again is expensive and unnecessary. Pressing the icon button or right
+ * clicking/long pressing on the item should trigger an action that displays the
+ * menu on the location of the mouse.
+ */
+function ActionMenu({
+  store,
+  path,
+  name,
+  isFile,
+  isSelected,
+}: ActionMenuProps) {
+  const dispatch = useAppDispatch();
+
+  return (
+    <Menu
+      trigger={(props) => {
+        return (
+          <Pressable {...props}>
+            <Icon
+              as={FontAwesome5}
+              name="ellipsis-h"
+              accessibilityLabel="actions"
+              opacity={props ? 0.8 : 1}
+              height="100%"
+              size={4}
+            />
+          </Pressable>
+        );
+      }}
+    >
+      <Menu.Item
+        onPress={() => {
+          if (isSelected) {
+            dispatch(
+              storageSlice.actions.unmarkSelected({
+                store,
+                name,
+                path,
+              }),
+            );
+          } else {
+            dispatch(
+              storageSlice.actions.markSelected({
+                store,
+                name,
+                isFile,
+                path,
+              }),
+            );
+          }
+        }}
+      >
+        {isSelected ? "Unselect" : "Select"}
+      </Menu.Item>
+      <Menu.Item
         onPress={() => {
           dispatch(
             storageSlice.actions.promptAction({
               type: StorageAction.Rename,
-              name: item.name,
+              isFile,
+              name,
               path,
               store,
-              isFile: item.is_file,
             }),
           );
         }}
-      />
-    </HStack>
+      >
+        Rename
+      </Menu.Item>
+      <Menu.Item
+        onPress={() => {
+          dispatch(
+            storageSlice.actions.promptAction({
+              type: StorageAction.Delete,
+              isFile,
+              name,
+              path,
+              store,
+            }),
+          );
+        }}
+      >
+        Delete
+      </Menu.Item>
+    </Menu>
   );
 }
