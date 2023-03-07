4cba7f0f47e4ef04a054855045f5b7b356d4545b
Kaan Barmore-Genc <kaan@bgenc.net>
2023-03-07 06:13:22 +0000

    Fix spinner to overlap correctly

--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -2,13 +2,13 @@ import { IconLoader2 } from "@tabler/icons-react";
 
 export function Spinner() {
   return (
-    <>
+    <div style={{ position: "relative" }}>
       <IconLoader2 className="animate-spin w-12 h-12" />
       <IconLoader2
-        style={{ position: "absolute" }}
+        style={{ position: "absolute", top: 8, left: 8 }}
         className="animate-spin-counter-clockwise w-8 h-8"
       />
-    </>
+    </div>
   );
 }
 
