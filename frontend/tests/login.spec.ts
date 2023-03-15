475df6bb11ecdbcc45f2300d04c53201c847b657
Kaan Barmore-Genc <kaan@bgenc.net>
2023-03-15 04:22:21 +0000

    Re-enable skipped e2e test

--- frontend/tests/login.spec.ts
+++ frontend/tests/login.spec.ts
@@ -1,12 +1,11 @@
 import { test, expect } from "@playwright/test";
 import { login } from "./common";
 
-// TODO: Re-enable once login errors are displayed
-test.skip("Logging in with an incorrect password fails", async ({ page }) => {
+test("Logging in with an incorrect password fails", async ({ page }) => {
   await login({ page, username: "testuser", password: "wrong" });
 
   expect(page.locator("text=Log in")).toBeTruthy();
-  expect(page.locator("text=Bad username or password")).toBeTruthy();
+  expect(page.locator("text=username or password is incorrect")).toBeTruthy();
 });
 
 test("Logging in with the right password works", async ({ page }) => {
