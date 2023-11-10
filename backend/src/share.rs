bd509aee4e32f2369b1a2afefb9173121d42f6f7
Kaan <kaan@bgenc.net>
2023-11-09 23:21:32 -0600

    hack together sharing backend

--- backend/src/share.rs
+++ backend/src/share.rs
@@ -1,10 +1,14 @@
 use actix_web::{
-    get, route,
+    delete, get, route,
     web::{self, ReqData},
     HttpResponse,
 };
 use nanoid::nanoid;
-use sea_orm::{ActiveModelTrait, ColumnTrait, EntityTrait, QueryFilter, Set, TransactionTrait};
+use sea_orm::{
+    ActiveModelTrait, ColumnTrait, EntityTrait, ModelTrait, QueryFilter, QuerySelect, Set,
+    TransactionTrait,
+};
+use serde::Deserialize;
 use tracing_unwrap::{OptionExt, ResultExt};
 
 use crate::{
@@ -78,5 +82,125 @@ async fn create_share(
             Ok(HttpResponse::Ok().body(""))
         }
         _ => return Err(StorageError::NotAuthorized),
+    }
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+#[cfg_attr(feature = "generate_types", derive(TypeDef))]
+pub struct Share {
+    pub path: String,
+    pub by_user: String,
+    pub token: String,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+#[cfg_attr(feature = "generate_types", derive(TypeDef))]
+pub struct GetSharesResponse {
+    pub shares: Vec<Share>,
+    pub bookmark: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct GetSharesQuery {
+    bookmark: Option<String>,
+}
+
+const SHARES_COUNT: u64 = 50;
+
+#[tracing::instrument]
+#[get("/shares")]
+async fn get_shares(
+    state: web::Data<AppState>,
+    authentication: ReqData<Authentication>,
+    query: web::Query<GetSharesQuery>,
+) -> HttpResponse {
+    match &authentication.user {
+        Some(user) => {
+            let mut shares_query = sharing::Entity::find()
+                .filter(sharing::Column::SharedWithUserId.eq(&user.user_id))
+                .limit(SHARES_COUNT);
+            if let Some(bookmark) = &query.bookmark {
+                shares_query = shares_query.filter(sharing::Column::Id.gt(bookmark))
+            }
+            let shares = shares_query
+                .all(&state.db)
+                .await
+                .unwrap_or_log()
+                .iter()
+                .map(|s| Share {
+                    path: s.path.to_owned(),
+                    by_user: s.shared_by_user_id.to_owned(),
+                    token: s.id.to_owned(),
+                })
+                .collect::<Vec<Share>>();
+
+            let bookmark = if (shares.len() as u64) < SHARES_COUNT {
+                None
+            } else {
+                Some(shares.last().unwrap().path.to_owned())
+            };
+            HttpResponse::Ok().json(GetSharesResponse { shares, bookmark })
+        }
+        None => HttpResponse::Unauthorized().body(""),
+    }
+}
+
+#[tracing::instrument]
+#[get("/shares/{share_path:.*}")]
+async fn get_share_by_path(
+    state: web::Data<AppState>,
+    authentication: ReqData<Authentication>,
+    share_path: web::Path<String>,
+) -> HttpResponse {
+    match &authentication.user {
+        Some(user) => {
+            let share = sharing::Entity::find()
+                .filter(sharing::Column::Path.eq(share_path.to_string()))
+                .filter(sharing::Column::SharedWithUserId.eq(&user.user_id))
+                .one(&state.db)
+                .await
+                .unwrap_or_log();
+
+            if let Some(share) = share {
+                HttpResponse::Ok().json(Share {
+                    by_user: user.username.to_owned(),
+                    path: share.path.to_owned(),
+                    token: share.id.to_owned(),
+                })
+            } else {
+                HttpResponse::NotFound().body("")
+            }
+        }
+        None => HttpResponse::Unauthorized().body(""),
+    }
+}
+
+#[tracing::instrument]
+#[delete("/shares/{share_path:.*}")]
+async fn delete_share(
+    state: web::Data<AppState>,
+    authentication: ReqData<Authentication>,
+    share_path: web::Path<String>,
+) -> HttpResponse {
+    match &authentication.user {
+        Some(user) => {
+            let tx = state.db.begin().await.unwrap_or_log();
+            let share = sharing::Entity::find()
+                .filter(sharing::Column::Path.eq(share_path.to_string()))
+                .filter(sharing::Column::SharedByUserId.eq(&user.user_id))
+                .one(&tx)
+                .await
+                .unwrap_or_log();
+
+            if let Some(share) = share {
+                share.delete(&tx).await.unwrap_or_log();
+                tx.commit().await.unwrap_or_log();
+                HttpResponse::Ok().body("")
+            } else {
+                tx.commit().await.unwrap_or_log();
+                HttpResponse::NotFound().body("")
+            }
+        }
+        None => HttpResponse::Unauthorized().body(""),
     }
 }
